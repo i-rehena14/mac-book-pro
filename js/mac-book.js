a4fdb87f4ac39502187568d7c5a6afcfa6c5bb6e
i-rehena14 <rehenaa14@gmail.com>
2021-08-21 21:36:56 +0600

    promocode function added

--- js/mac-book.js
+++ js/mac-book.js
@@ -3,33 +3,39 @@ function setExtraPrice(costId, setCost) {
     const initialPrice = document.getElementById(costId);
     initialPrice.innerText = setCost;
     const getCostText = parseFloat(initialPrice.innerText);
-    // console.log(getCostText);
-
     const total = totalPrice();
 
 }
 //get innerText
-/* function getInnerText(){
-    const bestPrice = document.getElementById('best-price');
-    const bestPriceText = bestPrice.innerText;
-} */
+function getInnerText(price) {
+    const innerPrice = document.getElementById(price);
+    const innerPriceText = innerPrice.innerText;
+    return innerPriceText;
+}
 //total price
 function totalPrice() {
-    const bestPrice = document.getElementById('best-price');
-    const bestPriceText = bestPrice.innerText;
-    const memoryCost = document.getElementById('memory-cost');
-    const memoryCostText = memoryCost.innerText;
-    const storageCost = document.getElementById('storage-cost');
-    const storageCostText = storageCost.innerText;
-    const deliveryCost = document.getElementById('delivery-cost');
-    const deliveryCostText = deliveryCost.innerText;
-    const previousTotalPrice = document.getElementById('total-price');
+    const bestPriceText = getInnerText('best-price');
+    const memoryCostText = getInnerText('memory-cost');
+    const storageCostText = getInnerText('storage-cost');
+    const deliveryCostText = getInnerText('delivery-cost');
 
+    const previousTotalPrice = document.getElementById('total-price');
     const finalTotalPrice = parseFloat(bestPriceText) + parseFloat(memoryCostText) + parseFloat(storageCostText) + parseFloat(deliveryCostText);
     const bottomTotal = document.getElementById('bottom-total');
     previousTotalPrice.innerText = finalTotalPrice;
     bottomTotal.innerText = finalTotalPrice;
     return finalTotalPrice;
+}
+//get promo-code
+function getPromoCode() {
+    const promoCode = document.getElementById('promo-code');
+    const promoCodeValue = promoCode.value;
+    if (promoCodeValue == 'stevekaku') {
+        let bottomPrice = totalPrice();
+        bottomPrice = bottomPrice - bottomPrice * .2;
+        document.getElementById('bottom-total').innerText = bottomPrice;
+        promoCode.value = '';
+    }
 }
 
 //8GB memory price
@@ -41,11 +47,11 @@ document.getElementById('sixteen-gb').addEventListener('click', function () {
     setExtraPrice('memory-cost', 180);
 });
 
-//256 Storage cost
+//256GB Storage cost
 document.getElementById('256GB').addEventListener('click', function () {
     setExtraPrice('storage-cost', 0);
 });
-//512 Storage cost
+//512GB Storage cost
 document.getElementById('512GB').addEventListener('click', function () {
     setExtraPrice('storage-cost', 100);
 });
@@ -61,19 +67,9 @@ document.getElementById('free').addEventListener('click', function () {
 document.getElementById('charged').addEventListener('click', function () {
     setExtraPrice('delivery-cost', 20);
 });
-//promo code
+//apply for promo code
 document.getElementById('apply-button').addEventListener('click', function () {
-    const promoCode = document.getElementById('promo-code');
-    const promoCodeValue = promoCode.value;
-    if (promoCodeValue == 'stevekaku') {
-        let bottomPrice = totalPrice();
-        bottomPrice = bottomPrice - bottomPrice * .2;
-        document.getElementById('bottom-total').innerText = bottomPrice;
-        promoCode.value = '';
-    }
-    else {
+    getPromoCode();
 
-    }
-
-})
+});
 
